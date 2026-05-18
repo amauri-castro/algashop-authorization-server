bf34f2d2d7b1720d25c118f2fb8b519dcfb56ffe
Amauri Castro <amauri.p.castro@gmail.com>
2026-05-18 08:10:06 -0300

    configurado armazenamento do consentimento do usuario

--- src/main/java/com/algashop/authorizationserver/config/PersistenceConfig.java
+++ src/main/java/com/algashop/authorizationserver/config/PersistenceConfig.java
@@ -3,6 +3,7 @@ package com.algashop.authorizationserver.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationConsentService;
 import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 
@@ -15,5 +16,13 @@ public class PersistenceConfig {
             RegisteredClientRepository registeredClientRepository
     ) {
         return  new JdbcOAuth2AuthorizationService(jdbcTemplate, registeredClientRepository);
+    }
+
+    @Bean
+    public JdbcOAuth2AuthorizationConsentService auth2AuthorizationConsentService(
+            JdbcTemplate jdbcTemplate,
+            RegisteredClientRepository registeredClientRepository
+    ) {
+        return new JdbcOAuth2AuthorizationConsentService(jdbcTemplate, registeredClientRepository);
     }
 }
