c16b7cf2e730670561489d9dc077b6913b03cb78
Amauri Castro <amauri.p.castro@gmail.com>
2026-07-18 19:43:24 -0300

    configuracao de cookie para silent refresh

--- src/main/java/com/algashop/authorizationserver/infrastructure/security/AuthorizationServerSecurityConfig.java
+++ src/main/java/com/algashop/authorizationserver/infrastructure/security/AuthorizationServerSecurityConfig.java
@@ -26,6 +26,7 @@ public class AuthorizationServerSecurityConfig {
 
     private final OidcUserInfoMapper oidcUserInfoMapper;
     private final OidcLogoutAuthenticationSuccessHandler oidcLogoutAuthenticationSuccessHandler;
+    private final AlgaShopSecurityProperties properties;
 
     @Bean
     @Order(1)
@@ -34,6 +35,10 @@ public class AuthorizationServerSecurityConfig {
 
         http.securityMatcher(authorizationServer.getEndpointsMatcher())
                 .cors(Customizer.withDefaults())
+                .headers(headers -> {
+                    var csp = properties.getCsp();
+                    headers.contentSecurityPolicy(c -> c.policyDirectives(csp.getPolicyDirectives()));
+                })
                 .with(authorizationServer, configurer ->
                     configurer.oidc(oidc -> oidc
                             .logoutEndpoint(logout -> logout.logoutResponseHandler(oidcLogoutAuthenticationSuccessHandler))
---
+++ src/main/java/com/algashop/authorizationserver/infrastructure/security/cookie/CookieConfig.java
@@ -0,0 +1,26 @@
+package com.algashop.authorizationserver.infrastructure.security.cookie;
+
+import com.algashop.authorizationserver.infrastructure.security.AlgaShopSecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+
+@Configuration
+public class CookieConfig {
+
+    @Bean
+    public CookieSerializer cookieSerializer(AlgaShopSecurityProperties properties) {
+        var serializer = new DefaultCookieSerializer();
+        serializer.setCookieName("JSESSIONID");
+        serializer.setCookiePath("/");
+        serializer.setUseHttpOnlyCookie(true);
+
+        var cookieProperties = properties.getCookie();
+        serializer.setUseSecureCookie(cookieProperties.getSecure());
+        serializer.setSameSite(cookieProperties.getSameSite());
+        serializer.setDomainName(cookieProperties.getDomainName());
+
+        return serializer;
+    }
+}
