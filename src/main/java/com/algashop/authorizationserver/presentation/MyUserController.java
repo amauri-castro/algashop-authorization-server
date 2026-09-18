1678a87335665305ab1fc97b7563de1f27cc817d
Amauri Castro <amauri.p.castro@gmail.com>
2026-09-17 22:28:39 -0300

    endpoints para alteração de senha

--- src/main/java/com/algashop/authorizationserver/presentation/MyUserController.java
+++ src/main/java/com/algashop/authorizationserver/presentation/MyUserController.java
@@ -1,11 +1,13 @@
 package com.algashop.authorizationserver.presentation;
 
 import com.algashop.authorizationserver.application.security.SecurityChecks;
+import com.algashop.authorizationserver.application.user.management.PasswordManagementApplicationService;
 import com.algashop.authorizationserver.application.user.query.AuthUserOutput;
 import com.algashop.authorizationserver.application.user.query.AuthUserQueryService;
 import com.algashop.authorizationserver.infrastructure.security.check.SecurityAnnotations;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +20,7 @@ public class MyUserController {
 
     private final SecurityChecks securityCheck;
     private final AuthUserQueryService authUserQueryService;
+    private final PasswordManagementApplicationService passwordManagementApplicationService;
 
     @GetMapping
     @SecurityAnnotations.CanAccessOwnProfile
@@ -25,4 +28,11 @@ public class MyUserController {
         UUID authenticatedUserId = securityCheck.getAuthenticatedUserId();
         return authUserQueryService.findById(authenticatedUserId);
     }
+
+    @PostMapping("/password-change")
+    @SecurityAnnotations.CanAccessOwnProfile
+    public void requestPasswordChange() {
+        passwordManagementApplicationService.requestPasswordChange(securityCheck.getAuthenticatedUserId());
+    }
+
 }
